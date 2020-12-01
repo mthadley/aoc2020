d4a6db236ce2bf89d352ebf2bac6342e025b3fd6
Michael Hadley <mikethadley@gmail.com>
2020-11-30 21:33:14 -0800

    Day 1, part 1

--- lib/advent_of_code/day1.rb
+++ lib/advent_of_code/day1.rb
@@ -1,9 +1,22 @@
 class AdventOfCode::Day1
+  TARGET = 2020
   def part1
-    "foo"
+    sum_map = input.each_with_object({}) do |n, map|
+      map[TARGET - n] = n
+    end
+
+    first = input.find { |n| sum_map.has_key?(n) }
+    second = sum_map[first]
+
+    first * second
   end
 
   def part2
-    "bar"
+  end
+
+  private
+
+  def input
+    @input ||= File.readlines("input/day1.txt").map(&:to_i)
   end
 end
