3c6c6e6a41129e5a2dcc4d805de455615930695c
Michael Hadley <mikethadley@gmail.com>
2020-11-30 22:19:18 -0800

    Day 1, part 2

--- lib/advent_of_code/day1.rb
+++ lib/advent_of_code/day1.rb
@@ -1,20 +1,38 @@
 class AdventOfCode::Day1
   TARGET = 2020
   def part1
-    sum_map = input.each_with_object({}) do |n, map|
-      map[TARGET - n] = n
+    find_two_sum(TARGET).inject(:*)
+  end
+
+  def part2
+    input.each do |n|
+      possible_nums = find_two_sum(TARGET - n)
+
+      if possible_nums
+        return [n, *possible_nums].inject(:*)
+      end
     end
+
+    -1
+  end
+
+  private
+
+  def find_two_sum(target)
+    sum_map = build_sum_map(target)
 
     first = input.find { |n| sum_map.has_key?(n) }
     second = sum_map[first]
 
-    first * second
+    result = [first, second]
+    result if result.all?
   end
 
-  def part2
+  def build_sum_map(target)
+    input.each_with_object({}) do |n, map|
+      map[target - n] = n
+    end
   end
-
-  private
 
   def input
     @input ||= File.readlines("input/day1.txt").map(&:to_i)
