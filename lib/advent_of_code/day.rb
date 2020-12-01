7976caf2b74c06a1748a269f3cc0811b1b2e2f48
Michael Hadley <mikethadley@gmail.com>
2020-11-30 22:40:16 -0800

    Should be nested

--- lib/advent_of_code/day.rb
+++ lib/advent_of_code/day.rb
@@ -1,30 +1,32 @@
-class Day
-  def self.input_file(name, &block)
-    @@input_filename = name
-    @@input_parser = block
-  end
+class AdventOfCode
+  class Day
+    def self.input_file(name, &block)
+      @@input_filename = name
+      @@input_parser = block
+    end
 
-  def part_one
-    "N/A"
-  end
+    def part_one
+      "N/A"
+    end
 
-  def part_two
-    "N/A"
-  end
+    def part_two
+      "N/A"
+    end
 
-  protected
+    protected
 
-  def input
-    @input ||=
-      begin
-        file_path = File.expand_path("../../input/#{@@input_filename}", __dir__)
-        content = File.readlines(file_path)
+    def input
+      @input ||=
+        begin
+          file_path = File.expand_path("../../input/#{@@input_filename}", __dir__)
+          content = File.readlines(file_path)
 
-        if @@input_parser.nil?
-          content
-        else
-          @@input_parser.call(content)
+          if @@input_parser.nil?
+            content
+          else
+            @@input_parser.call(content)
+          end
         end
-      end
+    end
   end
 end
