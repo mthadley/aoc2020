840912b592c415b2e9bc380c9efe00a448d37a27
Michael Hadley <mikethadley@gmail.com>
2020-12-06 23:01:57 -0800

    Day7 part 1

---
+++ lib/advent_of_code/day7.rb
@@ -0,0 +1,130 @@
+require "strscan"
+require "set"
+
+module AdventOfCode
+  class Day7 < Day
+    has_input_file
+
+    part1 answer: 248 do
+      rules.colors_needed_for(Bag.new(condition: "shiny", color: "gold"))
+    end
+
+    private
+
+    def rules
+      @rules ||= RuleGraph.new(input.map { RuleParser.parse!(_1) })
+    end
+
+    class RuleGraph
+      def initialize(rules)
+        @edges = {}
+
+        rules.each do |rule|
+          @edges[rule.key] = rule.deps
+        end
+      end
+
+      def colors_needed_for(bag)
+        colors = Set.new
+        seen = Set.new
+
+        @edges.keys.each do |start_bag|
+          next if start_bag == bag
+
+          if seen.member?(start_bag)
+            colors.add(start_bag)
+          elsif path = path_to(bag, current: start_bag)
+            seen |= path
+            colors.add(start_bag)
+          end
+        end
+
+        colors.size
+      end
+
+      def path_to(needle, current:, seen: Set.new)
+        edges = @edges[current]
+
+        return seen if needle == current
+        return nil if edges.empty?
+
+        edges.each do |edge|
+          next if seen.member?(edge.bag)
+
+          if path = path_to(needle, current: edge.bag, seen: Set[edge.bag] | seen)
+            return path
+          end
+        end
+
+        nil
+      end
+    end
+
+
+    Rule = Struct.new(:key, :deps, keyword_init: true)
+    Bag = Struct.new(:condition, :color, keyword_init: true)
+    BagCount = Struct.new(:bag, :count, keyword_init: true)
+
+    class RuleParser
+      def self.parse!(line)
+        new(StringScanner.new(line)).parse!
+      end
+
+      def initialize(scanner)
+        @s = scanner
+      end
+
+      def parse!
+        key_bag = BagParser.new(@s).parse!
+
+        fail ArgumentError, "Expected a bag at #{@s.pos}" unless key_bag
+
+        deps =
+          if @s.match?(/ contain no other bags./)
+            []
+          else
+            parse_deps!
+          end
+
+        Rule.new(key: key_bag, deps: deps)
+      end
+
+      def parse_deps!
+        @s.skip(/\s+contain\s+/)
+
+        deps = []
+        until @s.eos?
+          deps << parse_dep!
+        end
+
+        deps
+      end
+
+      def parse_dep!
+        @s.scan(/(?<count>\d+)\s+/) or fail ArgumentError, "Expected count: \"#{@s.rest}\""
+        count = @s[:count]
+
+        bag = BagParser.new(@s).parse!
+
+        @s.scan(/,\s+|\./) or fail ArgumentError, "Expected period or comma: \"#{@s.rest}\""
+
+        BagCount.new(bag: bag, count: count)
+      end
+    end
+
+    class BagParser
+      BAG_FORMAT = /(?<condition>\w+)\s+(?<color>\w+)\s+bags?/
+
+      def initialize(string_scanner)
+        @s = string_scanner
+      end
+
+      def parse!
+        @s.scan BAG_FORMAT or fail ArgumentError, "Invalid bag: #{@s.rest}"
+
+        Bag.new(condition: @s[:condition], color: @s[:color])
+      end
+    end
+
+  end
+end
