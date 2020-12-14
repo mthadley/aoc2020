2b66457b19b6fa9bbbd545a95ce0dfa191965795
Michael Hadley <mikethadley@gmail.com>
2020-12-14 11:49:35 -0800

    Day 14 part 2

--- lib/advent_of_code/day14.rb
+++ lib/advent_of_code/day14.rb
@@ -1,14 +1,20 @@
 module AdventOfCode
   class Day14 < Day
-    input_file do |lines|
-      lines.map { Instruction.parse!(_1) }
-    end
+    has_input_file
 
     part1 answer: 4886706177792 do
+      get_mem_values(Instruction)
+    end
+
+    part2 answer: 3348493585827 do
+      get_mem_values(InstructionV2)
+    end
+
+    private
+
+    def get_mem_values(ins_class)
       program = Program.new
-
-      input.each { |instruction| instruction.run(program) }
-
+      input.each { |line| ins_class.parse!(line).run(program) }
       program.mem.values.sum
     end
 
@@ -17,7 +23,6 @@ module AdventOfCode
 
       def initialize
         @mem = {}
-        @mask = Mask.new("0")
       end
     end
 
@@ -48,13 +53,14 @@ module AdventOfCode
 
     class Instruction
       def self.parse!(string)
-        SetMask.parse(string) ||
-          SetMem.parse(string) or
+        const_get(:SetMask).parse(string) ||
+          const_get(:SetMem).parse(string) or
           fail ArgumentError, "Not a valid instruction: \"#{string}\""
       end
 
       class SetMask
         FORMAT = /^mask = (?<mask>\w+)$/
+        attr_reader :mask
 
         def self.parse(string)
           if match = FORMAT.match(string)
@@ -67,12 +73,13 @@ module AdventOfCode
         end
 
         def run(program)
-          program.mask = Mask.new(@mask)
+          program.mask = Mask.new(mask)
         end
       end
 
       class SetMem
         FORMAT = /^mem\[(?<loc>\d+)\] = (?<val>\d+)$/
+        attr_reader :loc, :val
 
         def self.parse(string)
           if match = FORMAT.match(string)
@@ -86,7 +93,50 @@ module AdventOfCode
         end
 
         def run(program)
-          program.mem[@loc] = program.mask.apply(@val)
+          program.mem[loc] = program.mask.apply(val)
+        end
+      end
+    end
+
+    class Decoder
+      def initialize(mask_s)
+        @mask = mask_s
+      end
+
+      def apply(loc)
+        loc_chars = loc.to_s(2).rjust(@mask.size, "0").chars
+        loc_tmpl =
+          @mask.chars.zip(loc_chars).map do |mask_c, loc_c|
+            case mask_c
+            when "X" then "X"
+            when "1" then "1"
+            when "0" then loc_c
+            end
+          end.join
+
+        [0, 1].repeated_permutation(loc_tmpl.count("X")).map do |perm|
+          i = 0
+          loc_tmpl.gsub(/X/) do
+            num = perm[i]
+            i += 1
+            num
+          end
+        end
+      end
+    end
+
+    class InstructionV2 < Instruction
+      class SetMask < Instruction::SetMask
+        def run(program)
+          program.mask = Decoder.new(mask)
+        end
+      end
+
+      class SetMem < Instruction::SetMem
+        def run(program)
+          program.mask.apply(loc).each do |address|
+            program.mem[address] = val
+          end
         end
       end
     end
