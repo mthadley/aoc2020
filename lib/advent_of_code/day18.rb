4a797524b5555fe43bae5af0481a4fff574d5a4b
Michael Hadley <mikethadley@gmail.com>
2020-12-19 12:44:34 -0800

    Day 18 part 2

--- lib/advent_of_code/day18.rb
+++ lib/advent_of_code/day18.rb
@@ -5,30 +5,54 @@ module AdventOfCode
     has_input_file
 
     part1 answer: 12918250417632 do
-      input.sum { |line| evaluate(line) }
+      input.sum { |line| evaluate(line, parser: MathParser) }
+    end
+
+    part2 answer: 171259538712010 do
+      input.sum { |line| evaluate(line, parser: MathWithPrecedenceParser) }
     end
 
     private
 
-    def evaluate(str)
-      MathTransformer.new.apply(MathParser.new.parse(str))
+    def evaluate(str, parser:)
+      MathTransformer.new.apply(parser.new.parse(str))
     end
 
     class MathParser < Parslet::Parser
+      PRECEDENCE = {
+        multiply: 1,
+        add: 1
+      }
+
       rule(:space) { match("\s").repeat(1) }
       rule(:integer) { match("[0-9]").repeat(1).as(:integer) }
       rule(:operator) { match("[+*]") }
       rule(:operation) {
         infix_expression(
           integer | parens,
-          [space >> str('*').as(:op) >> space, 1, :left],
-          [space >> str('+').as(:op) >> space, 1, :left]
+          [
+            space >> str('*').as(:op) >> space,
+            self.class.const_get(:PRECEDENCE).fetch(:multiply),
+            :left
+          ],
+          [
+            space >> str('+').as(:op) >> space,
+            self.class.const_get(:PRECEDENCE).fetch(:add),
+            :left
+          ]
         )
       }
       rule(:parens) { str("(") >> expr >> str(")") }
       rule(:expr) { operation | parens | integer }
 
       root(:expr)
+    end
+
+    class MathWithPrecedenceParser < MathParser
+      PRECEDENCE = {
+        multiply: 1,
+        add: 2
+      }
     end
 
     class MathTransformer < Parslet::Transform
