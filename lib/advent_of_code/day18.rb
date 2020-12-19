bbc906b4abb74ccffef344c39dc0ed57a018c876
Michael Hadley <mikethadley@gmail.com>
2020-12-19 15:24:49 -0800

    Use a single class

--- lib/advent_of_code/day18.rb
+++ lib/advent_of_code/day18.rb
@@ -5,54 +5,44 @@ module AdventOfCode
     has_input_file
 
     part1 answer: 12918250417632 do
-      input.sum { |line| evaluate(line, parser: MathParser) }
+      input.sum { |line| evaluate(line, parser: MathParser.new) }
     end
 
     part2 answer: 171259538712010 do
-      input.sum { |line| evaluate(line, parser: MathWithPrecedenceParser) }
+      input.sum { |line| evaluate(line, parser: MathParser.new(precedence: {add: 2})) }
     end
 
     private
 
     def evaluate(str, parser:)
-      MathTransformer.new.apply(parser.new.parse(str))
+      MathTransformer.new.apply(parser.parse(str))
     end
 
     class MathParser < Parslet::Parser
-      PRECEDENCE = {
-        multiply: 1,
-        add: 1
-      }
-
       rule(:space) { match("\s").repeat(1) }
       rule(:integer) { match("[0-9]").repeat(1).as(:integer) }
       rule(:operator) { match("[+*]") }
       rule(:operation) {
         infix_expression(
           integer | parens,
-          [
-            space >> str('*').as(:op) >> space,
-            self.class.const_get(:PRECEDENCE).fetch(:multiply),
-            :left
-          ],
-          [
-            space >> str('+').as(:op) >> space,
-            self.class.const_get(:PRECEDENCE).fetch(:add),
-            :left
-          ]
+          [space >> str('*').as(:op) >> space, precedence_for(:multiply), :left],
+          [space >> str('+').as(:op) >> space, precedence_for(:add), :left ]
         )
       }
       rule(:parens) { str("(") >> expr >> str(")") }
       rule(:expr) { operation | parens | integer }
 
       root(:expr)
-    end
 
-    class MathWithPrecedenceParser < MathParser
-      PRECEDENCE = {
-        multiply: 1,
-        add: 2
-      }
+      def initialize(precedence: {})
+        @precedence = {add: 1, multiply: 1}.merge(precedence)
+      end
+
+      private
+
+      def precedence_for(op)
+        @precedence.fetch(op) || 1
+      end
     end
 
     class MathTransformer < Parslet::Transform
